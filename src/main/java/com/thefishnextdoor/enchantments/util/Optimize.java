e917aceb52764f46ad84500fb40316e0d1324320
TheFishNextDoor <unconfigured@null.spigotmc.org>
2023-12-08 21:50:36 -0600

    null check

--- src/main/java/com/thefishnextdoor/enchantments/util/Optimize.java
+++ src/main/java/com/thefishnextdoor/enchantments/util/Optimize.java
@@ -5,6 +5,7 @@ import org.bukkit.inventory.ItemStack;
 public class Optimize {
 
     public static boolean couldHaveCustomEnchantment (ItemStack item) {
+        if (item == null) return false;
         return item.hasItemMeta() && item.getItemMeta().hasLore();
     }
 }
