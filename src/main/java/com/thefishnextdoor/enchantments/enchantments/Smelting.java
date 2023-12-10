134b25bbe5e9b22c610d772a8731eaabc5b2ef57
TheFishNextDoor <unconfigured@null.spigotmc.org>
2023-12-10 12:44:55 -0600

    refactor crafting util

--- src/main/java/com/thefishnextdoor/enchantments/enchantments/Smelting.java
+++ src/main/java/com/thefishnextdoor/enchantments/enchantments/Smelting.java
@@ -4,19 +4,29 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentTarget;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.RecipeChoice.ExactChoice;
+import org.bukkit.inventory.RecipeChoice.MaterialChoice;
 
 import com.thefishnextdoor.enchantments.CustomEnchantment;
-import com.thefishnextdoor.enchantments.util.CraftingUtil;
 import com.thefishnextdoor.enchantments.util.EnchantUtil;
 import com.thefishnextdoor.enchantments.util.InventoryUtil;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 
 public class Smelting extends Enchantment {
+
+    public static HashMap<Material, Material> smeltingMap = Smelting.generateSmeltingMap();
 
     public Smelting(NamespacedKey key) {
         super(key);
@@ -62,18 +72,62 @@ public class Smelting extends Enchantment {
         if (item == null) return false;
         return InventoryUtil.isTool(item.getType());
     }
-
+    
     public static void onBlockDropItems(Player player, List<Item> drops) {
         if (!EnchantUtil.holding(player, CustomEnchantment.SMELTING)) return;
         for (Item drop : drops) {
-            CraftingUtil.smelt(drop);
+            Smelting.smelt(drop);
         }
     }
 
     public static void onBlockDropItems(Player player, Collection<ItemStack> drops) {
         if (!EnchantUtil.holding(player, CustomEnchantment.SMELTING)) return;
         for (ItemStack drop : drops) {
-            CraftingUtil.smelt(drop);
+            Smelting.smelt(drop);
         }
+    }
+
+    private static void smelt(Item item) {
+        ItemStack itemStack = item.getItemStack();
+        Material material = itemStack.getType();
+        Material smelted = Smelting.smelted(material);
+        if (smelted == null) return;
+        itemStack.setType(smelted);
+        item.setItemStack(itemStack);
+    }
+
+    private static void smelt(ItemStack itemStack) {
+        Material material = itemStack.getType();
+        Material smelted = Smelting.smelted(material);
+        if (smelted == null) return;
+        itemStack.setType(smelted);
+    }
+
+    private static Material smelted(Material material) {
+        return smeltingMap.get(material);
+    }
+
+    private static HashMap<Material, Material> generateSmeltingMap() {
+        HashMap<Material, Material> smeltingMap = new HashMap<>();
+        Iterator<Recipe> recipes = Bukkit.recipeIterator();
+        while (recipes.hasNext()) {
+            Recipe recipe = recipes.next();
+            if (!(recipe instanceof FurnaceRecipe)) continue;
+            FurnaceRecipe furnaceRecipe = (FurnaceRecipe) recipe;
+            RecipeChoice inputChoice = furnaceRecipe.getInputChoice();
+            Material resultMaterial = furnaceRecipe.getResult().getType();
+            if (inputChoice instanceof MaterialChoice) {
+                MaterialChoice materialChoice = (MaterialChoice) inputChoice;
+                for (Material material : materialChoice.getChoices()) {
+                    smeltingMap.put(material, resultMaterial);
+                }
+            } else if (inputChoice instanceof ExactChoice) {
+                ExactChoice exactChoice = (ExactChoice) inputChoice;
+                for (ItemStack itemStack : exactChoice.getChoices()) {
+                    smeltingMap.put(itemStack.getType(), resultMaterial);
+                }
+            }
+        }
+        return smeltingMap;
     }
 }
