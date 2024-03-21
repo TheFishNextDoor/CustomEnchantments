54dd048e568052646a01a9aaf179eff6dc97d26a
TheFishNextDoor <unconfigured@null.spigotmc.org>
2024-03-20 20:07:25 -0500

    correctly get Bukkit enchantment when registering

--- src/main/java/com/thefishnextdoor/customenchantments/NMS.java
+++ src/main/java/com/thefishnextdoor/customenchantments/NMS.java
@@ -3,6 +3,7 @@ package com.thefishnextdoor.customenchantments;
 import java.lang.reflect.Field;
 import java.util.IdentityHashMap;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.craftbukkit.v1_20_R3.inventory.CraftItemStack;
 
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -63,8 +64,15 @@ public class NMS {
 
     public static org.bukkit.enchantments.Enchantment registerEnchantment(CustomEnchantment enchantment) {
         NMSEnchantment nmsEnchantment = new NMSEnchantment(enchantment);
+
         net.minecraft.core.Registry.register(BuiltInRegistries.ENCHANTMENT, enchantment.getKey().getKey(), nmsEnchantment);
-        return org.bukkit.Registry.ENCHANTMENT.get(enchantment.getKey());
+
+        NamespacedKey key = NamespacedKey.minecraft(enchantment.getKey().getKey());
+        org.bukkit.enchantments.Enchantment bukkitEnchantment = org.bukkit.Registry.ENCHANTMENT.get(key);
+        if (bukkitEnchantment == null) {
+            throw new IllegalStateException("Failed to register enchantment " + enchantment.getKey());
+        }
+        return bukkitEnchantment;
     }
 
     private static boolean setFieldValue(Object of, String fieldName, Object value) {
